14f4ecab0abd14713f86698c2fff241ab0a79c47
matinfate <matinfatemi528@gmail.com>
2026-09-17 15:41:27 +0330

    feat(audio): add global sound volume

--- sound_manager.py
+++ sound_manager.py
@@ -3,6 +3,7 @@ import  arcade
 class SoundManager:
 
     def __init__(self):
+        self.volume = 0.2
         self.shoot_sound= arcade.load_sound("assets/sound/shoot.wav")
         self.enemy_death_sound= arcade.load_sound("assets/sound/enemy_death.wav")
         self.power_up_sound= arcade.load_sound("assets/sound/power_up.wav")
@@ -11,19 +12,19 @@ class SoundManager:
         self.game_over_sound= arcade.load_sound("assets/sound/game_over.wav")
 
     def play_shoot(self):
-        arcade.play_sound(self.shoot_sound)
+        arcade.play_sound(self.shoot_sound,volume=self.volume)
 
     def play_enemy_death(self):
-        arcade.play_sound(self.enemy_death_sound)
+        arcade.play_sound(self.enemy_death_sound,volume=self.volume)
 
     def play_power_up(self):
-        arcade.play_sound(self.power_up_sound)
+        arcade.play_sound(self.power_up_sound,volume=self.volume)
 
     def play_player_hit(self):
-        arcade.play_sound(self.player_hit_sound)
+        arcade.play_sound(self.player_hit_sound,volume=self.volume)
 
     def play_wave_start(self):
-        arcade.play_sound(self.wave_start_sound)
+        arcade.play_sound(self.wave_start_sound,volume=self.volume)
 
     def play_game_over(self):
-        arcade.play_sound(self.game_over_sound)
+        arcade.play_sound(self.game_over_sound,volume=self.volume)
